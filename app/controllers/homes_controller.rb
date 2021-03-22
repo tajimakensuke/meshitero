0b18a7a55f3bb3fa8c6e819dd50b7de9398df712
tajimakensuke <kenyuu25@gmail.com>
2021-03-22 09:26:28 +0000

    [add]3

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,11 @@
+class HomesController < ApplicationController
+
+def top
+end
+
+
+
+
+
+end
+
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c9fd95884577806344f5afa5cd659a7147e5cd1fdffbe04e52fd2ab17d3355cb747890743fa44b99c7606673791b06ba05d12516ba16eb7521d7b7850bcc1e1f'
