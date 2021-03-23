128a8d664691905f09856d24d12bbe923550e1b0
tajimakensuke <kenyuu25@gmail.com>
2021-03-23 13:04:37 +0000

    [add]5-profileimage

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,9 +5,9 @@ class PostImage < ApplicationRecord
 	has_many :post_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 
+  def favorited_by?(user)
 
-	def favorited_by?(user)
-	favorites.where(user_id: user.id).exists?
-	end
+    favorites.where(user_id: user.id).exists?
+  end
 
 end
